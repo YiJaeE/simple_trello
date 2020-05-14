1c876706de957507bdbffd3afcf65c881e3a626f
YiJaeE <jinnn2181@gmail.com>
2020-05-14 17:50:57 +0900

    feat: auto create uniq board box

--- src/Board.js
+++ src/Board.js
@@ -2,17 +2,41 @@ import React, { useState } from 'react';
 import './Board.css';
 
 const Board = ({ userId, userLogOut }) => {
-  const [boardTitle, setBoardTitle] = useState([
-    '하위',
-    '바위',
-    '안뇽',
-    '잘가',
-    '얼마나 해야 더 길어지나',
-    '하나만 더 해본다',
-    '머가 문제야 ㅠㅠㅠㅠㅠ',
-  ]);
-  const [newBoard, setNewBoard] = useState(false);
-  console.log(boardTitle);
+  const [boardTitle, setBoardTitle] = useState([]);
+
+  const [todo, setTodo] = useState([]);
+
+  const generateId = () =>
+    boardTitle.length
+      ? Math.max(...boardTitle.map((board) => board.id)) + 1
+      : 1;
+
+  const generateBoard = (e) => {
+    const newBoard = {
+      id: generateId(),
+      title: e.target.value,
+    };
+
+    if (e.key === 'Enter') {
+      setBoardTitle([...boardTitle, newBoard]);
+    }
+  };
+
+  const removeBoard = ({ target }) => {
+    const id = +target.parentNode.id;
+    setBoardTitle(boardTitle.filter((board) => id !== board.id));
+  };
+
+  const generateTodo = (e) => {
+    const newTodo = {
+      id: `Todo${generateId()}`,
+      content: e.target.value,
+    };
+    if (e.key === 'Enter') {
+      setTodo([...todo, newTodo]);
+    }
+    console.log(todo);
+  };
 
   return (
     <div className="boardContainer">
@@ -28,12 +52,24 @@ const Board = ({ userId, userLogOut }) => {
           type="text"
           className="newInputBoard"
           placeholder="새로운 보드 제목을 작성하세요"
+          onKeyPress={(e) => generateBoard(e)}
         />
       </section>
       <section className="boardSection">
-        {boardTitle.map((title, idx) => (
-          <ul key={`board${idx}`} className="boardCard">
+        {boardTitle.map(({ id, title }) => (
+          <ul id={id} key={`board${id}`} className="boardCard">
             {title}
+            <button type="button" className="removeBtn" onClick={removeBoard}>
+              보드 지우기
+            </button>
+            {todo.map(({ content, idx }) => (
+              <li key={idx}>{content}</li>
+            ))}
+            <input
+              type="text"
+              className="inputTodo"
+              onKeyPress={generateTodo}
+            />
           </ul>
         ))}
       </section>
