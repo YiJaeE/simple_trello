29afc113b079f03fae1a6bdc355c5bf4810c6057
YiJaeE <jinnn2181@gmail.com>
2020-06-11 17:32:12 +0900

    feat: Complement login rules

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,13 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import '../styles/Login.css';
 
 const Login = ({ setIsLogin, userId, setUserId }) => {
+  const [rightPW, setRightPW] = useState(false);
   const userInput = ['id', 'password'];
 
+  const regExpPW = value => {
+    const regExp = /^(?=.*[a-zA-Z])((?=.*\d)|(?=.*\W)).{8,20}$/;
+    return regExp.test(value) ? 1 : 0;
+  };
+
   const handlechange = (key, e) => {
-    if (key === 'id') {
-      setUserId(e.target.value);
-    }
+    key === 'id' && setUserId(e.target.value);
+    key === 'password' && regExpPW(e.target.value) ? setRightPW(true) : setRightPW(false);
   };
 
   const handleButtonClick = () => {
@@ -15,24 +20,21 @@ const Login = ({ setIsLogin, userId, setUserId }) => {
     setIsLogin(true);
   };
 
-  const handleKeyPress = (e) => {
-    if (e.target.matches('.password') && e.key === 'Enter') {
-      handleButtonClick();
-    }
+  const handleKeyPress = e => {
+    e.target.matches('.password') && e.key === 'Enter' && rightPW && handleButtonClick();
   };
 
   const handleBlur = (key, e) => {
-    const regExpPW = /^(?=.*\d)(?=.*[a-zA-Z])[0-9a-zA-Z]{8,10}$/;
-    if (key === 'password' && !regExpPW.test(e.target.value)) {
+    key === 'password' &&
+      !regExpPW(e.target.value) &&
       console.log(`형식에 맞는 비밀번호를 기입해주세요`);
-    }
   };
   return (
     <>
       <div className="login-container">
         <form className="form-container">
           <fieldset>
-            <legend>회원가입 폼</legend>
+            <legend>로그인 폼</legend>
             {userInput.map((input, idx) => (
               <label htmlFor={input} key={`input${idx}`}>
                 <span className="a11y-hidden">{input}</span>
@@ -40,13 +42,17 @@ const Login = ({ setIsLogin, userId, setUserId }) => {
                   type={input === 'password' ? 'password' : 'text'}
                   className={input}
                   placeholder={`${input}를 입력하세요`}
-                  onChange={(e) => handlechange(input, e)}
+                  onChange={e => handlechange(input, e)}
                   onKeyPress={handleKeyPress}
-                  onBlur={(e) => handleBlur(input, e)}
+                  onBlur={e => handleBlur(input, e)}
                 />
               </label>
             ))}
-            <button type="button" className="btn" onClick={handleButtonClick}>
+            <button
+              type="button"
+              className={rightPW ? 'btn-active' : 'btn'}
+              onClick={rightPW ? handleButtonClick : null}
+            >
               로그인
             </button>
           </fieldset>
