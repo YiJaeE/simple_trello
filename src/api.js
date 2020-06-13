6d51e3782822f88db7cc552f5bf69d106fb1541b
YiJaeE <47887717+YiJaeE@users.noreply.github.com>
2020-06-13 23:19:27 +0900

    fixed: correction of typos

--- src/api.js
+++ src/api.js
@@ -9,7 +9,7 @@ export const trelloApi = {
   getTodos: () => api.get('/todosState'),
   createBoard: (board) => api.post(`/boardsState/`, board).then((res) => res),
   removeBoard: (id) => api.delete(`/boardsState/${id}`).then((res) => res),
-  createTodo: (todos) => api.post(`todosState/`, todos).then((res) => res),
+  createTodo: (todos) => api.post(`/todosState/`, todos).then((res) => res),
   checkTodo: ({ id, completed }) =>
     api.patch(`/todosState/${id}`, { completed }).then((res) => res),
   removeTodo: (id) => api.delete(`/todosState/${id}`).then((res) => res),
